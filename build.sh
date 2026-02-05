41d39038e651697a8054ad438d21ab9f245031d6
lueluelue2006 <lueluelue2006@users.noreply.github.com>
2026-02-05 04:26:45 +0100

    Release v3.6.4

--- build.sh
+++ build.sh
@@ -71,9 +71,9 @@ cat > MusicPlayer.app/Contents/Info.plist << EOF
     <key>NSPrincipalClass</key>
     <string>MusicPlayerApplication</string>
 	<key>CFBundleShortVersionString</key>
-	<string>3.6.3</string>
+	<string>3.6.4</string>
 	    <key>CFBundleVersion</key>
-	    <string>3.6.3</string>
+	    <string>3.6.4</string>
     <key>LSMinimumSystemVersion</key>
     <string>13.0</string>
     <key>NSHighResolutionCapable</key>
